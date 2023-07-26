87d4aa485ee70c17662765c2332c7665aea50a74
abbliseng <54662679+abbliseng@users.noreply.github.com>
2023-07-26 23:01:44 +0200

    MVP - Swiper

--- hj-rnvilja/pages/foretag/index.tsx
+++ hj-rnvilja/pages/foretag/index.tsx
@@ -12,19 +12,28 @@ import Workshops from "../../components/Workshops";
 import FormsSection from "../../components/FormsSection";
 import { motion } from "framer-motion";
 
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
 
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { EffectCreative, Pagination } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/effect-creative';
-import 'swiper/css/pagination';
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-cards";
+
+// import required modules
+import { Autoplay, EffectCards, EffectCoverflow } from "swiper/modules";
+
+type Reference = {
+  img: string,
+  title: string,
+  description: string,
+}
 
 function SponsorLogo(props) {
   return (
     <motion.div
-        whileHover={{
-            scale: 1.1,
-        }}
+      whileHover={{
+        scale: 1.1,
+      }}
     >
       <FontAwesomeIcon
         color="var(--foretag)"
@@ -35,38 +44,101 @@ function SponsorLogo(props) {
   );
 }
 
-function SwiperReferences() {
+type SwiperReferencesProps = {
+  references: Reference[],
+}
+
+function SwiperReferences({ references }: SwiperReferencesProps) {
   return (
-    <Swiper
+    <div className='swiper-wrapper'>
+      <Swiper
+        effect={"cards"}
         grabCursor={true}
-        effect={'creative'}
-        creativeEffect={{
-          prev: {
-            shadow: true,
-            translate: ['-20%', 0, -1],
-          },
-          next: {
-            translate: ['100%', 0, 0],
-          },
+        loop={true}
+        autoplay={{
+          delay: 10000,
+          disableOnInteraction: false,
         }}
-        pagination={true}
-        modules={[EffectCreative, Pagination]}
-        className="mySwiper3"
+        modules={[Autoplay, EffectCards]}
+        className="mySwiper"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        {
+          references.map((reference) => {
+            return (
+              <SwiperSlide key={reference.title}>
+                <div className={styles.ref_card}>
+                  <div className={styles.ref_card_text_section}>
+                    <p className={styles.ref_card_text_title}>{reference.title}</p>
+                    <p className={styles.ref_card_text_description}>{reference.description}</p>
+                  </div>
+                  <div className={styles.ref_card_img_section}>
+                    <img className={styles.ref_card_img} src={reference.img} alt={reference.title} />
+                  </div>
+                </div>
+              </SwiperSlide>
+            )
+          })
+        }
       </Swiper>
+    </div>
   )
 }
 
-export default function BusinessPage({}) {
+export default function BusinessPage({ }) {
+  const references: Reference[] = [
+    {
+      img: "/hjarnvilja/idrott/sofie-gidlund.jpg",
+      title: "Sofie Gidlund – Skidåkare f.d alpin tävlingsåkare",
+      description: "Genom utvecklande samtal fyllda med massor av skratt, glädje och energi hjälpte Maria mig verkligen att förstå vem jag är och varför jag presterat som jag gjort. Trots att jag utövade en hyfsat okänd sport för henne engagerade hon sig på en gång och tog reda på allt hon behövde veta för att kunna hjälpa mig. Från start blev jag bemött med kärlek och omtänksamhet. Maria gav mig verktyg som hjälpte mig att förstå mitt tankesätt samt fick mig att uppskatta och vara nöjd med mig själv. Hon hjälpte mig alltså inte bara inom idrotten utan hjälpte mig även att växa som människa. Du fick mig att förstå vem jag är och vad jag vill göra. Så tack för att jag fick utforska hjärnans hemligheter med dig Maria!",
+    },
+    {
+      img: "/hjarnvilja/idrott/EAV.jpg",
+      title: "Ellinor Axelsson Vaughn – Skytte",
+      description: "Det är tack vare Maria som jag lyckades ta mig till mitt första Paralympics fyra år tidigare än vad jag trodde var möjligt! Det är otroligt vad mycket Maria gjort för mig under de två år vi har jobbat tillsammans. Marias mentala coachning gjorde att jag under mitt första Paralympics kunde sätta personbästa, och främst av allt vända en tävling som började i katastrof till att avsluta fantastiskt bra! Utan mental träning och coachning hade jag inte tagit mig så här långt som jag gjort på bara något år! Det ska bli spännande att se hur långt vi kan nå på fyra år till nästa Paralympics i Tokyo! ",
+    },
+    {
+      img: "/hjarnvilja/idrott/koping-ridklubb.png",
+      title: "Köpings ridklubb – tävlingssektionen",
+      description: "Maria höll en fantastisk föreläsning för oss. Hon guidade oss i tankens värld och förklarade hur vi kan vända begränsande tankar till positiva och möjliga! Det var så mycket energi, skratt och värde i de exempel och verktyg som Maria delade med sig av. Hon fångade oss alla! Oavsett ålder och tidigare erfarenheter kunde vi känna igen oss i Marias exempel och överföra dem till vår egen situation. Alla deltagare kommer att ta med sig sin egen mentala Barbapappa på nästa tävling.",
+    },
+    {
+      img: "",
+      title: "Anna Axberg – golf",
+      description: "Mental coaching med Maria har förändrat min syn på mig själv, min sport och mitt sätt att utöva den. Jag har lärt mig att ta kontroll över situationer jag förut inte kunde handskas med, exempelvis nervositet, stress och mitt humör. Maria har fått mig att förstå vad som har legat till grund för de blockeringar jag tidigare fått inför och på tävlingar och lärt mig hur jag ska gå till väga för häva dessa. Vi har tillsammans kommit fram till vad mitt syfte med golfen är, och arbetat med målsättningar som är anpassade just för mig. Jag upplever att jag har växt som människa och som idrottare både på det mentala och fysiska planet. Nu längtar jag bara till nästa säsong och ser fram emot ett långt samarbete med Maria!",
+    },
+    {
+      img: "",
+      title: "Anders Block – HLR-instruktör",
+      description: "Som instruktör av HLR möter man alla typer av människor; de som redan kan, de som har svårare att lära in, de som varit med om HLR på en kamrat, flamsiga grupper och allvarliga grupper. Maria har fått mig att känna mig trygg i det jag gör och på så vis får jag hög och jämn kvalitet på mina utbildningar. Jag har även fått hjälp med att hantera nervositet inför en utbildning. Så till dig som vill nå ut med ditt budskap inför en grupp människor kan jag verkligen rekommendera Maria på Hjärnvilja!",
+    },
+    {
+      img: "",
+      title: "Säljchef, Coop Forum",
+      description: "Genom sin kunskap i värdskap och mental träning fick Maria oss på Coop Forum Häggvik att baka in våra nytillkomna arbetsuppgifter i vardagen utan att de kändes betungande. Med vilja och rätt attityd tar vi nu oss an möjligheterna istället för att fastna på problemen. Maria har hjälpt oss att lyfta blicken och inte fastna i gamla invanda mönster. Tack! Ser fram emot fler tillfällen att jobba med Maria!",
+    },
+    {
+      img: "",
+      title: "Friskis & Svettis Västerås, spinning",
+      description: "Maria är en glad och entusiasmerande föreläsare som är lätt att förstå och fångade vårt intresse. Hon pratade om tankens kraft och om hur hjärnan omedvetet påverkar oss. Hur man med tankens kraft kan ta sig i stort hur långt som helst. Vi fick utöva några enkla övningar och efter dessa övningar var det inte svårt att övertyga oss om att tankens kraft verkligen fungerar.",
+    },
+    {
+      img: "",
+      title: "Sharon – hundsport",
+      description: "Att resonera och reflektera tillsammans med Maria om vikten av att utveckla mentala förmågor, har påverkat mitt tävlande i en väldigt positiv riktning. Maria har varit en fantastisk källa till inspiration och gett mig nyanserade mentala perspektiv, vilket hjälpt mig i jakten på mentalt hållbara strategier. Maria har även hjälpt mig att tänka mer komplext och långsiktigt kring målsättningar. Otroligt tacksam för Marias coachning!",
+    },
+    {
+      img: "",
+      title: "VGF",
+      description: "Vi har tränat hårt men alltid hamnat precis utanför pallen. Marias mentala träning har hjälpt tjejerna att prestera det där lilla extra som krävdes för att vi skulle ta brons på Svenska Cupen 2010.",
+    },
+    {
+      img: "",
+      title: "Winnet",
+      description: "Tack Maria för att du på ett mycket inspirerande, proffsigt, lustfyllt och handgripligt (!) sätt fick oss att ta fram den mentala styrkan inom oss. Du fick mig att inse vikten av att vara sams med mig själv, att ledarskap handlar om så mycket mer än att peka med hela handen.",
+    }
+  ]
+
+
   return (
     <main className={styles.container}>
       <div className={styles.left}></div>
@@ -80,7 +152,7 @@ export default function BusinessPage({}) {
           minHeight: "300px",
           position: "relative",
         }}>
-          <SwiperReferences />
+          <SwiperReferences references={references}/>
         </div>
         <div className={styles.break}></div>
         <div
@@ -92,11 +164,11 @@ export default function BusinessPage({}) {
             justifyContent: "space-evenly",
           }}
         >
-        <SponsorLogo icon={faImage} />
-        <SponsorLogo icon={faSpaghettiMonsterFlying} />
-        <SponsorLogo icon={faWandMagicSparkles} />
-        <SponsorLogo icon={faShoppingCart} />
-        <SponsorLogo icon={faHippo} />
+          <SponsorLogo icon={faImage} />
+          <SponsorLogo icon={faSpaghettiMonsterFlying} />
+          <SponsorLogo icon={faWandMagicSparkles} />
+          <SponsorLogo icon={faShoppingCart} />
+          <SponsorLogo icon={faHippo} />
         </div>
         <div className={styles.break}></div>
         <h1>Utbildningar</h1>
